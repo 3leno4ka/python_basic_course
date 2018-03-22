1e69a6bc87c67af66763a9d45d336ad68d605b66
Hennadii Tarasenko <hennadii.tarasenko@gmail.com>
2018-03-22 21:56:46 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,22 @@
+my_var = -10
+print(type(my_var))
+my_float = 23.67
+print(type(my_float))
+my_list = ['gh', 'gh', 'ghgfhh']
+print(type(my_list))
+my_tuple = (123, 'asd', my_var, 1)
+print(type(my_tuple))
+my_dict = {
+    "key1": "value1",
+    "key2": "value2"
+}
+print(type(my_dict))
+my_string = 'mama'
+print(type(my_string))
+my_char = '1'
+print(type(my_char))
+name = input('enter your name: ')
+number = input('enter your number: ')
+
+print('name: ' + name)
+print('number: ' + number)
