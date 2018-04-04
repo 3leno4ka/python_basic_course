252c56ceddb09addaf69bd8d6c5fe56470851b2a
Olena Sapielnikova <sapelnikova.eu@gmail.com>
2018-04-04 18:26:22 +0300

    hw with functions

--- l5_function/homework/homework5.py
+++ l5_function/homework/homework5.py
@@ -1,8 +1,8 @@
-from cc_validation import card_has_errors, print_bank, date_has_error, cvv_has_error
+from .cc_validation import card_has_errors, print_bank, date_has_error, cvv_has_error
 
-import os
-CARD_TYPE = os.environ.get('CARD_TYPE')
-print(CARD_TYPE)
+# import os
+# CARD_TYPE = os.environ.get('CARD_TYPE')
+# print(CARD_TYPE)
 
 while True:
     cc_number = input("Enter cc number in format \'XXXX XXXX XXXX XXXX\': \n")
@@ -12,8 +12,6 @@ while True:
 
 print_bank(cc_number)
 
-# except ValueError:
-#     print("Cc number must contain only numbers!")
 while True:
     cc_date_str = input("Enter cc expiration date in format \"mm/yy\":")   # экранирование специальных символов
     if date_has_error(cc_date_str):
